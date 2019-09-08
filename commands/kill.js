d5315a7c25335f84fb1d75adefe9dbffae328dec
christian <54652899+setotes@users.noreply.github.com>
2019-09-07 19:44:46 -0500

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const botconfig = require('../botconfig.json')
 
 module.exports.run = async  (bot,message, args) => {
 let user;
@@ -21,7 +22,7 @@ var url = gifs[Math.floor(Math.random() * gifs.length)];
     var url = gifs[Math.floor(Math.random() * gifs.length)];
  var embed = new Discord.RichEmbed()
     embed.setDescription(response);
-    embed.setColor(159039);
+    embed.setColor(botconfig.forestgreen);
     embed.setImage(url);
     embed.setFooter(`Viridi`)
     embed.setTimestamp();
