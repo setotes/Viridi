88f478117e0d7b8dd6feba9eaf86c43e378243da
christian <54652899+setotes@users.noreply.github.com>
2019-09-07 19:47:05 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js')
+const botconfig = require('../botconfig.json')
 
 module.exports.run = async (bot,message, args) => {
     const startTime = Date.now();
     var embed = new Discord.RichEmbed();
-    embed.setColor(159039)
+    embed.setColor(botconfig.forestgreen)
     embed.setDescription('Pong: ###ms');
     message.channel.send(embed)
     .then(message => {
         const endTime = Date.now();
         var embed2 = new Discord.RichEmbed();
-        embed2.setColor(159039)
+        embed2.setColor(botcondig.forestgreen)
         embed2.setDescription(`Pong: **${endTime - startTime}ms**`);
         message.edit(embed2);
     });
