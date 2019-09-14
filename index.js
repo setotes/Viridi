5521daf4b0cc5e6d82d92c965ecb5e44c126db32
christian <54652899+setotes@users.noreply.github.com>
2019-09-14 16:51:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,33 @@ const fs = require('fs');
 const xp = require("./xp.json");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
 fs.readdir('./commands/', (err, files) => {
     if(err) console.log(err);
     let jsfile = files.filter(f => f.split('.').pop() === 'js');
     if(jsfile.length <= 0){
         console.log('Couldnt Find command'); return;
     }
-    jsfile.forEach((f, i ) =>{
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded`);
-        bot.commands.set(props.help.name, props);
-    })
+    jsfile.forEach((f, i) => {
+      let pull = require(`./commands/${f}`);
+      bot.commands.set(pull.help.name, pull);  
+      pull.help.aliases.forEach(alias => {
+          bot.aliases.set(alias, pull.help.name)
+      });
+  });
 });
     bot.on('ready', function(){
         bot.user.setActivity(`${bot.guilds.size} people`, {type: 'WATCHING'})
         console.log(`${bot.user.username} is online on ${bot.guilds.size} servers`)
     });
-bot.login(token); 
+bot.login(botconfig.token); 
 
 bot.on('message', function(message){
     if(message.author.bot) return;
     if(message.channel.type === 'dm') return;
     let messageArray = message.content.split(' ');
+
     let xpAdd = Math.floor(Math.random() * 5) + 10;
     if(!xp[message.author.id]){
       xp[message.author.id] = {
@@ -41,9 +46,9 @@ bot.on('message', function(message){
       xp[message.author.id].level = curlvl + 1;
       let lvlup = new Discord.RichEmbed()
       .setTitle("Level Up!")
-      .setColor(159039)
+      .setColor(botconfig.forestgreen)
       .addField("New Level", curlvl + 1);
-      message.channel.send(lvlup)
+      message.channel.send(lvlup).then(msg => msg.delete(5000));
     }
     fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
       if(err) console.log(err)
@@ -52,6 +57,6 @@ bot.on('message', function(message){
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     if(!message.content.startsWith(botconfig.prefix)) return;
-        let cmdFile = bot.commands.get(cmd.slice(botconfig.prefix.length));
+        let cmdFile = bot.commands.get(cmd.slice(botconfig.prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(botconfig.prefix.length)))
     if(cmdFile) cmdFile.run(bot, message, args);
 });
