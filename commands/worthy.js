6b6ca6320dcd15b199aaefe62c6b31a1874a722e
christian <54652899+setotes@users.noreply.github.com>
2019-09-14 16:49:13 -0500

    Update worthy.js

--- commands/worthy.js
+++ commands/worthy.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const botconfig = require('../botconfig.json')
+
 module.exports.run = async (bot,message, args) => {
     var taest = [
         `${message.author.username}, you have been declared an enemy of the state. You are not worthy💀`,
@@ -9,11 +10,13 @@ module.exports.run = async (bot,message, args) => {
     var test = taest[Math.floor(Math.random() * taest.length)];
     let membed = new Discord.RichEmbed();
     membed.setAuthor(test)
-    membed.setColor(botconfig.green)
+    membed.setColor(botconfig.forestgreen)
     membed.setFooter('Viridi')
     membed.setTimestamp();
     message.channel.send(membed)
 }
 module.exports.help = {
-    name: 'worthy'
+    name: 'worthy',
+    description: 'Tests if you are worthy',
+    aliases: [""]
 }
